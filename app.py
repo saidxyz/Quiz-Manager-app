ee6d1bcfcba92942f0653dedaefffc69be189c97
Sas <90267986+SSA171@users.noreply.github.com>
2023-04-04 19:13:58 +0200

    Update templates and app.py

--- app.py
+++ app.py
@@ -12,41 +12,61 @@ app = Flask(__name__)
 def index():
     return render_template('home.html', the_title='Development page')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html', the_title='Login')
+
 
 @app.route('/register')
 def register():
     return render_template('register.html', the_title='Register')
 
+
 @app.route('/quiz')
 def quiz():
     return render_template('quiz.html', the_title='Quiz')
+
 
 @app.route('/results')
 def results():
     return render_template('results.html', the_title='Results')
 
+
 @app.route('/create_quiz')
 def create_quiz():
     return render_template('create_quiz.html', the_title='Create Quiz')
+
 
 @app.route('/create_question')
 def create_question():
     return render_template('create_question.html', the_title='Create Question')
 
-@app.route('/quizzes')
-def quizzes():
-    return render_template('quizzes.html', the_title='Quizzes')
 
 @app.route('/question')
 def question():
     return render_template('question.html', the_title='Question')
 
+
+@app.route('/quizzes')
+def quizzes():
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """SELECT * FROM quiz"""
+    cursor.execute(_SQL)
+    result = cursor.fetchall()
+    return render_template('admin/quizzes.html', the_title='Quizzes', quiz=result)
+
+
 @app.route('/all_results')
 def all_results():
-    return render_template('all_results.html', the_title='All Results')
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """SELECT * FROM results"""
+    cursor.execute(_SQL)
+    result = cursor.fetchall()
+    return render_template('admin/all_results.html', the_title='All Results', results=result)
+
 
 @app.route('/index')
 def hello() -> 'html':
@@ -67,4 +87,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
